5e5e31b9abf55948671b2e78c19dd758b3163dfb
mitchell <modipamitchell@gmail.com>
2023-02-17 12:51:59 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
 /**
@@ -13,10 +11,13 @@ int main(void)
 {
 int d;
 char low;
+
 for (d= '0'; d <= '9'; d++)
 putchar(d);
+
 for (low = 'a'; low <= 'f'; low++)
 putchar(low);
 putchar('\n');
+
 return (0);
 }
