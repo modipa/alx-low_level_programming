cb2d7a8e785fafe7a18179b84977c44236d6efcc
mitchell <modipamitchell@gmail.com>
2023-02-20 02:41:08 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,21 +2,19 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (succcess)
  */
 
 int main(void)
 {
-char c;
-
-c = 'a';
-while
-(c <= 'z')
-{
-if (( c != 'q' && c != 'e') && c <= 'z')
-putchar(c);
-c++;
-}
+	char c;
+	c = 'a';
+	while
+		(c <= 'z') {
+			if (( c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
 putchar('\n');
 return (0);
 }
